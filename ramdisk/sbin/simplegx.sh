ef9f408c2ca1b22e86828ccd2e3e1165a55337e4
AndroidGX <subut3x@gmail.com>
2016-02-02 12:40:57 +0100

    ramdisk: update simplegx script

--- ramdisk/sbin/simplegx.sh
+++ ramdisk/sbin/simplegx.sh
@@ -17,9 +17,23 @@ $BB rm -rf /data/lost+found/* 2> /dev/null;
 $BB rm -rf /data/tombstones/* 2> /dev/null;
 
 
+# ------------------
+# Initialize BusyBox
+# ------------------
+$BB chmod 06755 /system/xbin/busybox
+./system/xbin/busybox --install -s /system/xbin/
+
+
 # ---------------
 # Initialize root
 # ---------------
+if [ -e /system/xbin/su ]; then
+	$BB chmod 06755 /system/xbin/su;
+fi;
+if [ -e /system/xbin/daemonsu ]; then
+	$BB chmod 06755 /system/xbin/daemonsu;
+fi;
+
 /system/xbin/daemonsu --auto-daemon &
 
 
@@ -39,16 +53,16 @@ $BB run-parts /system/etc/init.d/
 # Google Services battery drain fixer by Alcolawl@xda
 # ---------------------------------------------------
 
-	pm enable com.google.android.gms/.update.SystemUpdateActivity
-	pm enable com.google.android.gms/.update.SystemUpdateService
-	pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
-	pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
-	pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
-	pm enable com.google.android.gsf/.update.SystemUpdateActivity
-	pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
-	pm enable com.google.android.gsf/.update.SystemUpdateService
-	pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
-	pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+pm enable com.google.android.gms/.update.SystemUpdateActivity
+pm enable com.google.android.gms/.update.SystemUpdateService
+pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
+pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+pm enable com.google.android.gsf/.update.SystemUpdateActivity
+pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
+pm enable com.google.android.gsf/.update.SystemUpdateService
+pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
+pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
 
 
 $BB mount -t rootfs -o remount,ro rootfs
